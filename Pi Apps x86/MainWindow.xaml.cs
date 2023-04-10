4c791d87be8e6ab6605e018caf7994ec15f37e60
Pakistaniboy442 <muhammadwhi@outlook.com>
2023-04-10 15:02:22 -0400

    Finishing Up Basic UI Control
    Yes.

--- Pi Apps x86/MainWindow.xaml.cs
+++ Pi Apps x86/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,18 +22,37 @@ namespace Pi_Apps_x86
     /// </summary>
     public partial class MainWindow : Window
     {
+        string picked = "no";
         public MainWindow()
         {
             InitializeComponent();
-            string selected = "Menu";
-
         }
-        
-        public void ButtonManager()
+
+        private void ListBoxItem_Selected(object sender, RoutedEventArgs e)
         {
-
+            Forward.IsEnabled = true;
+            this.picked = "Browser";
         }
 
+        private void ListBoxItem_Selected_1(object sender, RoutedEventArgs e)
+        {
+            Forward.IsEnabled = true;
+        }
 
+        private void ListBoxItem_Selected_2(object sender, RoutedEventArgs e)
+        {
+            Forward.IsEnabled = true;
+        }
+
+        private void ListBoxItem_Selected_3(object sender, RoutedEventArgs e)
+        {
+            Forward.IsEnabled = true;
+        }
+
+        private void Forward_Click(object sender, RoutedEventArgs e)
+        {
+            Menu.Visibility = Visibility.Hidden;
+            if (this.picked == "Browser") { this.Browsers.Visibility = Visibility.Visible; }
+        }
     }
 }
